a71450cfc17632438d5699ceea1086bf4f087a18
Mark van Kleef <mkleef@avans.nl>
2016-08-30 13:22:44 +0200

    Example of possible data structure

---
+++ DPA_Musicsheets/Notes/INote.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Factory;
+
+namespace DPA_Musicsheets.Notes
+{
+    public abstract class INote : ICloneable, IGetKey<String>
+    {
+
+        public INote()
+        {
+        }
+
+        public static INote create(String name)
+        {
+            return FactoryMethod<String, INote>.create(name);
+        }
+
+        public abstract String getKey();
+
+        public abstract object Clone();
+
+        public abstract void show();
+    }
+}

a1234e4cf63d2f8a4b29ac236fb9467cf052285f
Mark van Kleef <mkleef@avans.nl>
2016-09-05 21:58:20 +0200

    RestNote added, need to check implementation thou

---
+++ DPA_Musicsheets/Notes/RestNote.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.Notes
+{
+    class RestNote : INote
+    {
+        public RestNote()
+        {
+
+        }
+
+        public override string getKey()
+        {
+            return "R";
+        }
+
+        public override object Clone()
+        {
+            return new RestNote();
+        }
+
+        public override void show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

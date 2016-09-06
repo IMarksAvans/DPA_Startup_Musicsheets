a1428059d0e541de22e0b21dd9c542ea635adc9f
vegatarier <sam.sam.s@hotmail.com>
2016-09-06 14:48:06 +0200

    savers&readers

---
+++ DPA_Musicsheets/SaversReaders/MidiReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.SaversReaders
+{
+    class MidiReader : IReader
+    {
+        public int Load()
+        {
+            return 1;
+        }
+    }
+}
---
+++ DPA_Musicsheets/SaversReaders/MidiSaver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.SaversReaders
+{
+    class MidiSaver : ISaver
+    {
+        public int Save()
+        {
+            return 1;
+        }
+    }
+}

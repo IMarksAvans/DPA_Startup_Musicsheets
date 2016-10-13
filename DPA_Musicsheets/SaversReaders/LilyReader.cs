aad982cfc94531fef0ed9de844e164fe5a98f60f
Mark van Kleef <mkleef@avans.nl>
2016-10-13 12:44:39 +0200

    Alternative almost done

--- DPA_Musicsheets/SaversReaders/LilyReader.cs
+++ DPA_Musicsheets/SaversReaders/LilyReader.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace DPA_Musicsheets.SaversReaders
@@ -22,13 +23,9 @@ namespace DPA_Musicsheets.SaversReaders
         {
             string[] lines = System.IO.File.ReadAllLines(@"" + Filename);
 
-            // Display the file contents by using a foreach loop.
-            System.Console.WriteLine("Contents of " + Filename + " = ");
-            Debug.Print("something whon");
-            Debug.WriteLine(Filename);
-
-            foreach (string line in lines)
+            for (int i = 0; i < lines.Length; i++)
             {
+                String line = lines[i];
                 OurTrack track = new OurTrack();
                 track.Notes = new List<Note>();
 
@@ -54,8 +51,27 @@ namespace DPA_Musicsheets.SaversReaders
                 }
                 else if (line.Contains("repeat"))
                 {
-                    // get stack up to and including the alternative
-                    // convert
+                    List<string> repeatList = new List<string>(); 
+                    List<string> altList    = new List<string>();
+                    
+                    int repeatCount = Int32.Parse(Regex.Match(line, @"\d+").Value);
+
+                    i++;
+                    while (!line.Contains("alternative"))
+                    {
+                        line = lines[i];
+                        repeatList.Add(line);
+                        i++;
+                    }
+
+                    while (!line.Contains("}"))
+                    {
+                        line = lines[i];
+                        altList.Add(line);
+                        i++;
+                    }
+
+                    track.Notes.AddRange(readRepeat(repeatList, altList, repeatCount));
                 }
                 else if (line.Contains("{"))
                 {
@@ -87,47 +103,62 @@ namespace DPA_Musicsheets.SaversReaders
                     {
                         c = line[i];
                         if (Char.IsNumber(c))
-                        {
                             n.Duration = Int32.Parse(c.ToString());
-                        }
                         else
                         {
                             if (c.Equals('\''))
-                            {
                                 n.IncreaseOctave();
-                            }
                             else if (c.Equals(','))
-                            {
                                 n.DecreaseOctave();
-                            }
                             else if (c.Equals('.'))
-                            {
                                 n.Punt = true;
-                            }
                         }
 
                         i++;
-                        // ~ == door tot in de volgende maat
                     }
                     noteList.Add(n);
-                    // add the note to something.
                 }
             }
-            // r    = rest
             // dis  =
             // gis  =
-            // ~    =
-            // '    = increase note height
-            // .    = decrease note height
+            // ~    = door tot in de volgende maat
             // es   = mol
             // is   = kruis
 
             return noteList;
         }
 
-        private void readRepeat()
+        private List<Note> readRepeat(List<string> repeat, List<string> alt, int repeatCount)
         {
-            // please note the alternative, this replaced the first line with the second line.
+            List<Note> noteList     = new List<Note>();
+            List<Note> repeatList   = new List<Note>();
+            List<List<Note>> altList      = new List<List<Note>>();
+
+            foreach (string line in repeat)
+            {
+                repeatList.AddRange(readNoteLine(line));
+            }
+
+            foreach (string line in alt)
+            {
+                altList.Add(readNoteLine(line));
+            }
+
+            while (repeatCount > 0)
+            {
+                List<Note> toAdd = repeatList.ToList(); // Clone the list, to destroy the reference
+                
+                if (altList.Count() != 0)
+                {
+                    // logic to add the right alt
+                }
+
+                noteList.AddRange(toAdd);
+
+                repeatCount--;
+            }
+
+            return noteList;
         }
 
         public void LoadReserved()

3f473e99bfd19ea3b486b71fb2ee58a564e851e9
Mark van Kleef <mkleef@avans.nl>
2016-10-14 16:36:08 +0200

    Implemented the base of keyshortcuts

---
+++ DPA_Musicsheets/ChainOfResponsibility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DPA_Musicsheets
+{
+    class ChainOfResponsibility
+    {
+        public bool Handle(List<Key> keysDown)
+        {
+            return false;
+        }
+    }
+}
---
+++ DPA_Musicsheets/KeyShortcuts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DPA_Musicsheets
+{
+    class KeyShortcuts
+    {
+        private ChainOfResponsibility _actionChain = new ChainOfResponsibility();
+        private List<Key> _keysDown = new List<Key>();
+
+        private void textBox_keyDown(object sender, KeyEventArgs e)
+        {
+            _keysDown.Add(e.Key);
+            if (_actionChain.Handle(_keysDown))
+            {
+                e.Handled = true;
+                _keysDown.Clear();
+            }
+        }
+
+        private void textBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            _keysDown.Remove(e.Key);
+        }
+    }
+}

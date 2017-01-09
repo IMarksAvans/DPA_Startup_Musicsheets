f7b70451d4629f6c8efd366eb1df296bddcf4e81
vegatarier <sam.sam.s@hotmail.com>
2017-01-09 23:22:30 +0100

    CorPattern

---
+++ DPA_Musicsheets/CoR/KeyCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.CoR
+{
+    class KeyCommand
+    {
+        protected KeyCommand successor;
+
+        void Execute(string data)
+        {
+            if (successor != null)
+            {
+                successor.Execute(data);
+            }
+        }
+
+        void SetSuccessor(KeyCommand successor)
+        {
+            this.successor = successor;
+        }
+    }
+}

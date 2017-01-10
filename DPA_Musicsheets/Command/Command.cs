5dd4d2aae34c34d71730835c13e050182dddac79
vegatarier <sam.sam.s@hotmail.com>
2017-01-10 10:39:44 +0100

    commandpattern

---
+++ DPA_Musicsheets/Command/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.Command
+{
+    class Command
+    {
+        protected Receiver receiver;
+
+        internal void Execute()
+        {
+            
+        }
+    }
+}
---
+++ DPA_Musicsheets/Command/Invoker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.Command
+{
+    class Invoker
+    {
+        protected Command command;
+
+
+        public void Execute()
+        {
+            command.Execute();
+        }
+    }
+}

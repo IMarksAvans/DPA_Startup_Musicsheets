8dc0dd7278e9b2cde6802829bb15bfe2fef238d1
Mark van Kleef <mkleef@avans.nl>
2017-01-15 15:41:17 +0100

    Memento class prep

---
+++ DPA_Musicsheets/Memento/Memento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.Memento
+{
+    class Memento
+    {
+        private string _state;
+
+        // Constructor
+        public Memento(string state)
+        {
+            this._state = state;
+        }
+
+        // Gets or sets state
+        public string State
+        {
+            get { return _state; }
+        }
+    }
+}
---
+++ DPA_Musicsheets/Memento/Originator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.Memento
+{
+    class Originator
+    {
+        private string _state;
+
+        // Property
+        public string State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+            }
+        }
+
+        // Creates memento 
+        public Memento CreateMemento()
+        {
+            return (new Memento(_state));
+        }
+
+        // Restores original state
+        public void SetMemento(Memento memento)
+        {
+            State = memento.State;
+        }
+    }
+}
